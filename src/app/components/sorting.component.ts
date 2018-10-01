df663c50e6002f98baba036db5ca8125b76f8813
Narek29a <narek.torosyan29a@gmail.com>
2018-10-01 19:19:18 +0400

    git init along with remote origin set

---
+++ src/app/components/sorting.component.ts
@@ -0,0 +1,56 @@
+import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+
+@Component({
+  selector: 'sorting',
+  templateUrl: './sorting.component.html',
+  styleUrls: ['./sorting.component.css']
+
+})
+
+
+export class SortingComponent {
+
+  public numbers: Array<number> = [];
+  public validSize: boolean;
+  public isSend: boolean;
+  public bubble: Array<number> = [];
+  public selection: Array<number> = [];
+  public insertion: Array<number> = [];
+
+  constructor(private httpClient: HttpClient) {
+
+  }
+
+
+  public setNumbers(newValue) {
+    this.numbers.push(newValue);
+    if (this.numbers.length == 2) {
+      this.validSize = true;
+    }
+  }
+
+
+  public sort() {
+    this.isSend = true;
+    this.httpClient.post('http://localhost:8080/bubble/', {"numbers": this.numbers}).subscribe((data: any) => {
+      this.bubble = data;
+      console.log(this.bubble);
+    });
+    this.httpClient.post('http://localhost:8080/selection/', {"numbers": this.numbers}).subscribe((data: any) => {
+      this.selection = data;
+      console.log(this.selection);
+    });
+    this.httpClient.post('http://localhost:8080/insertion/', {"numbers": this.numbers}).subscribe((data: any) => {
+      this.insertion = data;
+      console.log(this.insertion);
+    });
+
+
+  }
+
+
+
+
+}
